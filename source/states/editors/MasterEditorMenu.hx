c86e71fe54628331ba837ca0a0a41d0eb8c1c09f
Vaesea <159406785+Vaesea@users.noreply.github.com>
2024-11-03 12:25:17 +0000

    Update MasterEditorMenu.hx

--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -29,7 +29,7 @@ class MasterEditorMenu extends MusicBeatState
 		FlxG.camera.bgColor = FlxColor.BLACK;
 		#if DISCORD_ALLOWED
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("Editors Main Menu", null);
+		DiscordClient.changePresence("Editors Menu", null);
 		#end
 
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
